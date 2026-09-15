60b6a0a786d78e5608aec6ffc196c020e6c37dd7
VigneshRaj727 <vigneshraj.rajasekar@adlinktech.com>
2026-09-15 19:12:37 +0530

    Add cross compilation support (#1) and version change
    * Add cross compilation support
    
    * Add Buildroot support 1
    
    * Update SEMA version to 4.4.5
    
    ---------
    
    Co-authored-by: VigneshRaj727 <vigneshraj.rajsekar@adlinktech.com>

--- sema.c
+++ sema.c
@@ -1,2 +1,7 @@
-'#define VERSION "4.4.4"' 
-'int main() { return 0; }' 
+#define VERSION "4.4.5"
+
+int main() {
+    printf("Customer cross compilation support");
+    printf("Buildroot support added");
+    return 0;
+}
